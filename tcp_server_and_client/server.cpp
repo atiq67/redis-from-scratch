9d566f11bd19c09b76a0fb5b68387e4dcec65a63
atiq67 <260136384+atiq67@users.noreply.github.com>
2026-02-15 18:44:28 +0000

    Add Simple TCP server and client

---
+++ tcp_server_and_client/server.cpp
@@ -0,0 +1,83 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static bool do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf)-1);
+    if(n < 0) {
+        msg("read() error");
+        return;
+    }
+
+    fprintf(stderr, "client says: %s\n", rbuf);
+    
+    // char wbuf[] = "world";
+    if(strcmp(rbuf, "stop")) {
+        char wbuf[] = "done";
+        write(connfd, wbuf, strlen(wbuf));
+        return false;
+    }
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+    return true;
+    
+    
+}
+
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0) {
+        die("socket()");
+    }
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);
+    int rv = bind(fd, (const struct sockaddr*)&addr, sizeof(addr));
+    if(rv) {
+        die("bind()");
+    }
+
+    rv = listen(fd, SOMAXCONN);
+    if(rv) {
+        die("listen()");
+    }
+
+    while(true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr*)&client_addr, &addrlen);
+        if(connfd < 0) {
+            continue;
+        }
+
+        bool res = do_something(connfd);
+        fprintf(stderr, "%d\n", res);
+        close(connfd);
+        if(!res) break;
+    }
+
+    return 0;
+}
